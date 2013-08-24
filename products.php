23a8ee370de82950c6d1fb00ca78d7961270126c
Rowland Mtetezi <rowlandmtetezi@gmail.com>
2013-08-24 12:23:37 +0300

    aded server

--- products.php
+++ products.php
@@ -1,25 +1,5 @@
 <?php
-/**
-* Copyright 2012 Facebook, Inc.
-*
-* You are hereby granted a non-exclusive, worldwide, royalty-free license to
-* use, copy, modify, and distribute this software in source code or binary
-* form for use in connection with the web services and APIs provided by
-* Facebook.
-*
-* As with any software that integrates with the Facebook platform, your use
-* of this software is subject to the Facebook Developer Principles and
-* Policies [http://developers.facebook.com/policy/]. This copyright notice
-* shall be included in all copies or substantial portions of the software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-* DEALINGS IN THE SOFTWARE.
-*/
+
 
   require 'server/fb-php-sdk/facebook.php';
 
@@ -27,7 +7,7 @@
   $app_secret = 'e2df834723ce92533fa64c2cff192d65';
   $app_namespace = 'cartooned';
   $app_url = 'http://apps.facebook.com/' . $app_namespace . '/';
-  $scope = 'email,publish_actions';
+  $scope = 'email,publish_actions,user_photos';
 
   // Init the Facebook SDK
   $facebook = new Facebook(array(
