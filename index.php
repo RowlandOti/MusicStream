0c0d6ef02a70c342260dd85c172bc023423e6926
Rowland Mtetezi <rowlandmtetezi@gmail.com>
2013-08-28 12:07:46 +0300

    aded

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
-<?php
 
-require_once('fblogin.php');
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" version="XHTML+RDFa 1.0" dir="ltr"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:dc="http://purl.org/dc/terms/"
+  xmlns:foaf="http://xmlns.com/foaf/0.1/"
+  xmlns:og="http://ogp.me/ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+  xmlns:sioc="http://rdfs.org/sioc/ns#"
+  xmlns:sioct="http://rdfs.org/sioc/types#"
+  xmlns:skos="http://www.w3.org/2004/02/skos/core#"
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema#">
 
-?>
-<!DOCTYPE html>
-<html>
-<head>
+<head profile="http://www.w3.org/1999/xhtml/vocab">
 <script src="//ads.lfstmedia.com/getad?site=194033" type="text/javascript"></script>
 <!--<script type="text/javascript" src="http://fileice.net/gateway/mygate.php?id=4365546a48424b486868637a586462334e4150634b673d3d"></script>-->
 <link href="main.css" rel="stylesheet" type="text/css" />
 <link href="generic.css" rel="stylesheet" type="text/css" />
+<!--tabcontent controls-->
+ <link href="tabs/template4/mctabs.css" rel="stylesheet" type="text/css" />
+<script src="tabs/javascript-tabs.js" type="text/javascript"></script> 
+<!--form controls-->
+<link href="form.css" rel="stylesheet" type="text/css" />
 
     <title>Cartoonise Your Facebook Profile Picture</title>
-
+    <!--slider controls-->
     <link href="themes/3/js-image-slider.css" rel="stylesheet" type="text/css" />
     <script src="themes/3/js-image-slider.js" type="text/javascript"></script>
 
@@ -35,9 +46,10 @@ require_once('fblogin.php');
 
 <body>
 
-<script src="http://connect.facebook.net/en_US/all.js"></script>
+<script src="http://connect.facebook.net/en_US/all.js">
+</script>
 
-   <script>
+<script>
 
      FB.init({ 
 
@@ -65,11 +77,16 @@ require_once('fblogin.php');
 
    </script>
 
- <script src="friends.js" type="text/javascript"></script>
+ <script src="friends.js" type="text/javascript">
+ </script>
 
 <center>
 
-<div id="wrap">
+<ul id="tabs1" class="mctabs"> <li><a href="#view1">Home</a></li> <li><a href="#view2">Portfolio</a></li> <li><a href="#view3">Upload</a></li> 
+</ul>
+<div id="wrap"> 
+<div class="panel-container"> 
+<div id="view1"> 
 <img src="images/stats/Banner.png" style="padding-left:50px;padding-bottom:30px" />
 
     <div id="sliderFrame">
@@ -89,14 +106,17 @@ require_once('fblogin.php');
 
         </div>
 
-      
+        
 
 <center>
 
 <h3><font color="#fa2853">Cartoonise Your Profile Picture</font></h3><img src="images/stats/Getnow.png" onClick="alert('You Must Invite All Friends To Cartoonise for free');sendRequestToFriends('Cartoonise Facebook Picture','Invite Your Friends Now');"/> 
 
 </center> 
-<div class="fb-subscribe" data-href="https://www.facebook.com/rowland.kk" data-show-faces="true" data-width="450"></div>
+<center>
+<div class="fb-subscribe" data-href="https://www.facebook.com/rowland.kk" data-show-faces="true" data-width="450">
+</div>
+</center> 
 
 
 <script type="text/javascript">
@@ -107,7 +127,103 @@ require_once('fblogin.php');
             slot: 'slot75486'
         });
     //]]>
-</script>
+</script> 
+</div> 
+<div id="view2">  
+
+</div> 
+<div id="view3"> 
+<form id=payment >
+<h1 id="top_name"></h1>
+	<fieldset>
+		<legend>Your details</legend>
+		<ol>
+			<li>
+				<label for=name>Name</label>
+				<input id=name name=name type=text placeholder="First and last name" required autofocus>
+			</li>
+			<li>
+				<label for=email>Email</label>
+				<input id=email name=email type=email placeholder="example@domain.com" required>
+			</li>
+			<li>
+				<label for=phone>Phone</label>
+				<input id=phone name=phone type=tel placeholder="Eg. +254710805009" required>
+			</li>
+			<li>
+				<label for=photo>Photo</label>
+				<input id=photo name=photo type=file placeholder="Select file" required>
+			</li>
+			<li>
+				<label for=photo>Message</label>
+				<textarea id=message name=message rows=3 required></textarea>
+			</li>
+		</ol>
+	</fieldset>
+	<!--<fieldset>
+		<legend>Delivery address</legend>
+		<ol>
+			<li>
+				<label for=address>Address</label>
+				<textarea id=address name=address rows=3 required></textarea>
+			</li>
+			<li>
+				<label for=postcode>Post code</label>
+				<input id=postcode name=postcode type=text required>
+			</li>
+			<li>
+				<label for=country>Country</label>
+				<input id=country name=country type=text required>
+			</li>
+		</ol>
+	</fieldset>
+	<fieldset disabled >
+		<legend>Card details</legend>
+		<ol>		
+			<li>
+				<fieldset>
+					<legend>Card type</legend>
+					<ol>
+						<li>
+							<input id=visa name=cardtype type=radio >
+							<label for=visa>VISA</label>
+						</li>
+						<li>
+							<input id=amex name=cardtype type=radio >
+							<label for=amex>AmEx</label>
+						</li>
+						<li>
+							<input id=mastercard name=cardtype type=radio >
+							<label for=mastercard>Mastercard</label>
+						</li>
+					</ol>
+				</fieldset>
+			</li>
+			<li>
+				<label for=cardnumber>Card number</label>
+				<input id=cardnumber name=cardnumber type=number required>
+			</li>
+			<li>
+				<label for=secure>Security code</label>
+				<input id=secure name=secure type=number required>
+			</li>
+			<li>
+				<label for=namecard>Name on card</label>
+				<input id=namecard name=namecard type=text placeholder="Exact name as on the card" required>
+			</li>
+		</ol>
+	</fieldset>-->
+	<fieldset>
+		<button type=submit>Buy it!</button>
+	</fieldset>
+</form> 
+</div> 
+</div>
+
+
+
+
+
 </div>
 </center>
 
