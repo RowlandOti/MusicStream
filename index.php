b0ea10788c3e9a645083d7fc738a22fc4a45016f
Rowland Mtetezi <rowlandotieno@gmail.com>
2013-10-09 19:19:34 -0700

    done no music

---
+++ index.php
@@ -0,0 +1,370 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" version="XHTML+RDFa 1.0" dir="ltr"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:dc="http://purl.org/dc/terms/"
+  xmlns:foaf="http://xmlns.com/foaf/0.1/"
+  xmlns:og="http://ogp.me/ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+  xmlns:sioc="http://rdfs.org/sioc/ns#"
+  xmlns:sioct="http://rdfs.org/sioc/types#"
+  xmlns:skos="http://www.w3.org/2004/02/skos/core#"
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema#">
+
+<head profile="http://www.w3.org/1999/xhtml/vocab">
+<script src="//ads.lfstmedia.com/getad?site=194033" type="text/javascript"></script>
+<!--<script type="text/javascript" src="http://fileice.net/gateway/mygate.php?id=4365546a48424b486868637a586462334e4150634b673d3d"></script>-->
+
+<link href="generic.css" rel="stylesheet" type="text/css" />
+<!--tabcontent controls-->
+<link href="tabcontent/template2/tabcontent.css" rel="stylesheet" type="text/css" />
+<script src="tabcontent/tabcontent.js" type="text/javascript"></script> 
+<!--form fbstyle controls-->
+<link href="form.css" rel="stylesheet" type="text/css" />
+<!--facebox controls-->
+<script src="overlay/jquery.tools.js" type="text/javascript"></script>
+<script src="overlay/overlay.js" type="text/javascript"></script>
+<link href="overlay/overlay.css" media="screen" rel="stylesheet" type="text/css"/>
+
+
+
+
+<title>Cartoonise Your Facebook Profile Picture</title>
+<!--slider controls-->
+<link href="themes/3/js-image-slider.css" rel="stylesheet" type="text/css" />
+<script src="themes/3/js-image-slider.js" type="text/javascript"></script>
+
+    
+
+
+</head>
+
+<body>
+
+<script src="http://connect.facebook.net/en_US/all.js">
+</script>
+
+   <script>
+
+     FB.init({ 
+
+       appId:'336953559728376', 
+
+	   cookie:true, 
+
+       status:true, 
+
+	   xfbml:true 
+
+     });
+
+	 
+
+	 function sendRequestViaMultiFriendSelector() {
+
+        FB.ui({method: 'apprequests',
+
+          message: 'Share it the news to your friends'
+
+        }, requestCallback);
+
+      }
+
+   </script>
+
+ <script src="friends.js" type="text/javascript">
+ </script>
+ <!--For shoutcast start-->
+<script type="text/javascript" src="js/swfobject.js"></script>	
+		<script type="text/javascript">
+
+			// JAVASCRIPT VARS
+			// the path to the SWF file
+			var swfPath = "preview.swf";
+			//swfPath += "?t=" + Date.parse(new Date()); // uncomment this line to activate cache buster		
+			
+			// stage dimensions
+			var stageW = 670;//560//"100%"; // minimum is 450
+			var stageH = 360;//400;//"100%"; // minimum is 260
+			
+			
+			// ATTRIBUTES
+		    var attributes = {};
+		    attributes.id = 'FlashComponent';
+		    attributes.name = attributes.id;
+		    
+			// PARAMS
+			var params = {};
+			params.bgcolor = "#333333";
+			params.allowfullscreen = "true";
+			params.allowScriptAccess = "always";			
+			//params.wmode = "transparent";
+			
+
+		    /* FLASH VARS */
+			var flashvars = {};
+			
+			/// if commented / delete these lines, the component will take the stage dimensions defined 
+			/// above in "JAVASCRIPT SECTIONS" section or those defined in the settings xml			
+			flashvars.componentWidth = stageW;
+			flashvars.componentHeight = stageH;							
+			
+			/// path to the content folder(where the xml files, images or video are nested)
+			/// if you want to use absolute paths(like "http://domain.com/images/....") then leave it empty("")
+            // Also, if you want the embed code to work correctly you'll need to set the an absolute path for pathToFiles, like this: http://www.yourwebsite.dom/.../mp3gallery/
+			flashvars.pathToFiles = "mp3gallery/";
+			flashvars.xmlPath = "xml/settings.xml";
+			flashvars.contentXMLPath = "xml/mp3gallery.xml";
+						
+			/** EMBED THE SWF**/
+			swfobject.embedSWF(swfPath, attributes.id, stageW, stageH, "9.0.124", "js/expressInstall.swf", flashvars, params, attributes);
+		</script>
+		<!--For shoutcast end-->
+		<!--Twitter-->
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<center>
+
+<ul class="tabs" data-persist="true"> <li  class="selected"><a href="#view1">Home</a></li> <li><a href="#view2">My Music</a></li> <li><a href="#view3">Upload</a></li> <li><a href="#view4">Sponsors</a></li> </ul>
+<div id="wrap"> 
+
+
+<div class="apple_overlay" id="overlay">
+  <!-- the external content is loaded inside this tag -->
+ 
+  <div class="contentWrap"></div>
+  
+</div>
+
+
+<div class="tabcontents"> 
+<div id="view1"> 
+
+<img src="images/stats/Banner.png" style="padding-left:50px;padding-bottom:30px" />
+
+    <div id="sliderFrame">
+
+        <div id="slider" >
+      
+	  <a href="http://www.flickr.com/photos/frf_kmeron/4967315055/" title="Janelle Monáe Live @ Warner Fall Session Botanique Bruxelles-0543 by Kmeron, on Flickr">
+            <img src="images/stats/KisenyaAPP.jpg"/>
+      </a>			
+			<img src="images/stats/YayeAPP.jpg"/>
+            <img src="images/stats/MuseveniAPP.jpg"/>
+            <img src="images/stats/SylvesterAPP.jpg"/>
+            <img src="images/stats/KibakiAPP.jpg"/>	
+			<img src="images/stats/JulietAPP.jpg"/>				
+			<img src="images/stats/NdaluAPP.jpg"/>
+            <img src="images/stats/RowlandAPP.jpg"/>
+
+        </div>
+ 
+        </div>
+
+        
+
+<center>
+
+<h3><font color="#fa2853">Cartoonise Your Profile Picture</font></h3><img src="images/stats/Getnow.png" onClick="alert('You Must Invite All Friends To Cartoonise for free');sendRequestToFriends('Cartoonise Facebook Picture','Invite Your Friends Now');"/> 
+
+</center> 
+<center>
+<div class="fb-subscribe" data-href="https://www.facebook.com/rowland.kk" data-show-faces="true" data-width="450">
+</div>
+</center> 
+
+
+<script type="text/javascript">
+    //<![CDATA[
+        LSM_Slot({
+            adkey: '17d',
+            ad_size: '468x60',
+            slot: 'slot75486'
+        });
+    //]]>
+</script> 
+</div> 
+<div id="view2">  
+
+	
+		<table width="100%" height="100%" cellpadding="0" cellspacing="0">
+			<td align="center">
+				
+				<!-- this div will be overwritten by SWF object -->		
+				<div id="FlashComponent">
+					<p>In order to view this object you need Flash Player 9+ support!</p>
+					<a href="http://www.adobe.com/go/getflashplayer">
+						<img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player"/>
+					</a>
+				</div>
+		
+			</td>
+		</table>
+		
+</div>
+<center>
+<div id="view3"> 
+
+<form id=payment >
+<h1 id="top_name"></h1>
+	<fieldset>
+		<legend>Your details</legend>
+		<ol>
+			<li>
+				<label for=name>Name</label>
+				<input id=name name=name type=text placeholder="First and last name" required autofocus>
+			</li>
+			<li>
+				<label for=email>Email</label>
+				<input id=email name=email type=email placeholder="example@domain.com" required>
+			</li>
+			<li>
+				<label for=phone>Phone</label>
+				<input id=phone name=phone type=tel placeholder="Eg. +254710805009" required>
+			</li>
+			<li>
+				<label for=photo>Photo</label>
+				<input id=photo name=photo type=file placeholder="Select file" required>
+			</li>
+			<li>
+				<label for=photo>Message</label>
+				<textarea id=message name=message rows=3 required></textarea>
+			</li>
+		</ol>
+	</fieldset>
+	<!--<fieldset>
+		<legend>Delivery address</legend>
+		<ol>
+			<li>
+				<label for=address>Address</label>
+				<textarea id=address name=address rows=3 required></textarea>
+			</li>
+			<li>
+				<label for=postcode>Post code</label>
+				<input id=postcode name=postcode type=text required>
+			</li>
+			<li>
+				<label for=country>Country</label>
+				<input id=country name=country type=text required>
+			</li>
+		</ol>
+	</fieldset>
+	<fieldset disabled >
+		<legend>Card details</legend>
+		<ol>		
+			<li>
+				<fieldset>
+					<legend>Card type</legend>
+					<ol>
+						<li>
+							<input id=visa name=cardtype type=radio >
+							<label for=visa>VISA</label>
+						</li>
+						<li>
+							<input id=amex name=cardtype type=radio >
+							<label for=amex>AmEx</label>
+						</li>
+						<li>
+							<input id=mastercard name=cardtype type=radio >
+							<label for=mastercard>Mastercard</label>
+						</li>
+					</ol>
+				</fieldset>
+			</li>
+			<li>
+				<label for=cardnumber>Card number</label>
+				<input id=cardnumber name=cardnumber type=number required>
+			</li>
+			<li>
+				<label for=secure>Security code</label>
+				<input id=secure name=secure type=number required>
+			</li>
+			<li>
+				<label for=namecard>Name on card</label>
+				<input id=namecard name=namecard type=text placeholder="Exact name as on the card" required>
+			</li>
+		</ol>
+	</fieldset>-->
+	<fieldset>
+		<button type=submit>Buy it!</button>
+	</fieldset>
+</form> 
+
+</div> 
+</center>
+<div id="view4">  
+<script type="text/javascript">
+    //<![CDATA[
+        LSM_Slot({
+            adkey: '1f8',
+            ad_size: '720x300',
+            slot: 'slot75813'
+        });
+    //]]>
+</script>
+</div>
+</div>
+
+
+
+
+
+</div>
+</center>
+
+<center>
+<div style="font-weight:bold;color:#707EBC;font-size:14px;">
+            <a href="faq.html" rel="#overlay" style="text-decoration:none"">Feedback</a><label style="margin-right:10px;color:#707EBC;">|</label>
+            <a href="faq.html" rel="#overlay" style="text-decoration:none">FAQ</a><label style="margin-right:10px;color:#707EBC;">|</label>
+            <a href="https://www.facebook.com/pages/Stats-Mtaani/395643417150270"  target="_blank" style="margin-right:10px;">Stats Mtaani Apps</a><label style="margin-right:10px;color:#707EBC;">|</label>
+            <a href="https://twitter.com/RowlandBiznez" class="twitter-follow-button" data-show-count="true" data-lang="en" style="margin-right:10px;">Follow @RowlandBiznez</a><label style="margin-right:10px;color:#707EBC;">
+      </div>
+<div style='font-size:11px;'>
+            <div style="margin-top:10px;">
+            If you are having a trouble loading Music player content, please update your Flash player by clicking <a href="http://www.adobe.com/products/flashplayer/" target="_blank"> HERE </a>.</div>
+
+            <div style="margin-top:10px;">
+            All graphic items including user interface, design, artwork & software  Copyright &copy; 2010-2013 <a href="https://www.facebook.com/pages/Stats-Mtaani/395643417150270" target="_blank">Stats Mtaani</a>.</div>
+            <div>All rights reserved.</div><br/>
+            <span>
+            <a href="http://agame.ntbmedia.com/agamedev.php/terms" target="_blank" style="margin-right:10px;">Terms of Service</a><label style="margin-right:10px;color:#707EBC;">|</label>
+                <a href="/privacypolicy" target="_blank" style="margin-right:10px;">Privacy Policy</a><label style="margin-right:10px;color:#707EBC;">|</label>
+                <a href="#"  style="margin-right:10px;" onclick="facebox_r('reportpop')" >Report Copyright</a>
+            </span>
+      </div>
+</center>
+
+</body>
+
+<script type="text/javascript">
+var aasd = false;
+function click2(){
+if (aasd == false) {
+alert("Invite Friends to Continue");
+}
+}
+</script>
+<script type="text/javascript">
+var aasd = false;
+function click1(){
+if (aasd == false) {
+alert("You miss this chance. Invite and get your credits!");
+}
+}
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34338753-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</html>
+
